838026d9f0b72a409f6cff6fd4da805e67a9e2a9
Oyeniyi Isaac <122137764+isaac0yen@users.noreply.github.com>
2024-01-13 10:52:42 +0100

    again

--- api/index.js
+++ api/index.js
@@ -1,3 +1,13 @@
-const app = require('../app.js');
+const express = require('express');
+const app = require('../app');
+const server = require('../bin/www');
 
-module.exports = app;
+module.exports = (req, res) => {
+  const { method, url } = req;
+  if (req.method === 'GET') {
+    server();
+    res.status(200).send('Server is running');
+  } else {
+    res.status(405).send('Method not allowed');
+  }
+};
